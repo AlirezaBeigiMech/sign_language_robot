38e2d2ccbfc5cdad6e7075a1481a17e8cfa2765f
Alireza Beigi <99119137+AlirezaBeigiMech@users.noreply.github.com>
2024-11-17 21:45:42 -0800

    Delete Docker directory

--- Docker/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# /etc/profile would be run before **login shell**, which can be used to set environment variables
-echo "export DISPLAY=$DISPLAY" >> /etc/profile
-echo "export QT_X11_NO_MITSHM=1" >> /etc/profile
-exec "$@"
